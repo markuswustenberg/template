087705a2bc32b5903f262a6632ca8066810a7bf2
Markus Wüstenberg <markus@wustenberg.dk>
2015-11-07 17:44:14 +0100

    Update React to 0.14.2 and update babelify use

---
+++ source/app.js
@@ -0,0 +1,6 @@
+import React from 'react' // eslint-disable-line no-unused-vars, because the JSX is translated to React.something
+import ReactDOM from 'react-dom'
+
+import Container from './components/container'
+
+ReactDOM.render(<Container/>, document.getElementById('container'))
---
+++ source/components/container.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class Container extends React.Component {
+  render() {
+    return <h1>❤ template</h1>
+  }
+}
