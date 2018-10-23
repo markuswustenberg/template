ae153d265b699d51dbba62478a34b767afb4fc24
Markus Wüstenberg <markus@wustenberg.dk>
2018-10-23 18:56:52 +0200

    Resurrect project and upgrade all the things

--- sources/components/home.js
+++ sources/components/home.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router'
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <h1>😎 template</h1>
-        <p>Routing:</p>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="about">About</Link></li>
-          <li><Link to="does-not-exist">Does not exist</Link></li>
-        </ul>
         <p>Redux initialized: {`${this.props.app.initialized}`}</p>
       </div>
     )
